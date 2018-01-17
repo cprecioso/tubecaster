a19f0fdf2f661ee4a087e2781c60ea3935ed6b10
Carlos Precioso <kalprestito@gmail.com>
2018-01-17 03:06:49 +0100

    Formatting

--- src/server/podcast.ts
+++ src/server/podcast.ts
@@ -13,39 +13,39 @@ app.get(
   "/" + routes.playlistPodcast(),
   apicache(`${60 + 59} min`),
   async (req, res) => {
-  const baseUrl = url.format({
-    protocol: req.protocol,
-    host: req.hostname,
-    pathname: req.baseUrl || "/"
-  })
-  const completeUrl = url.resolve(
-    baseUrl,
-    routes.playlistPodcast(req.params.playlistId)
-  )
-  const playlistInfo = await playlist(req.params.playlistId, {
-    key: process.env.YOUTUBE_API_KEY as string
-  })
-  const items = playlistItems(req.params.playlistId, {
-    key: process.env.YOUTUBE_API_KEY as string,
-    parts: [
-      PlaylistItems.Options.Part.Snippet,
-      PlaylistItems.Options.Part.Status
-    ]
-  })
+    const baseUrl = url.format({
+      protocol: req.protocol,
+      host: req.hostname,
+      pathname: req.baseUrl || "/"
+    })
+    const completeUrl = url.resolve(
+      baseUrl,
+      routes.playlistPodcast(req.params.playlistId)
+    )
+    const playlistInfo = await playlist(req.params.playlistId, {
+      key: process.env.YOUTUBE_API_KEY as string
+    })
+    const items = playlistItems(req.params.playlistId, {
+      key: process.env.YOUTUBE_API_KEY as string,
+      parts: [
+        PlaylistItems.Options.Part.Snippet,
+        PlaylistItems.Options.Part.Status
+      ]
+    })
 
-  const feed = await createFeed(
-    completeUrl,
-    (playlistId, itemId) => ({
-      url: url.resolve(baseUrl, routes.videoPlay(itemId)),
-      type: "video/mp4"
-    }),
-    playlistInfo,
-    items
-  )
+    const feed = await createFeed(
+      completeUrl,
+      (playlistId, itemId) => ({
+        url: url.resolve(baseUrl, routes.videoPlay(itemId)),
+        type: "video/mp4"
+      }),
+      playlistInfo,
+      items
+    )
 
-  res.contentType("text/xml")
-  res.send(feed)
-}
+    res.contentType("text/xml")
+    res.send(feed)
+  }
 )
 
 app.get("/" + routes.videoPlay(), async (req, res) => {
