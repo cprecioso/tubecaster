79e73b7f19075804d3ba179f96609fec5acabe25
Carlos Precioso <kalprestito@gmail.com>
2018-11-12 00:08:53 +0100

    Remove API key requirement

--- src/_config.ts
+++ src/_config.ts
@@ -1,11 +1,5 @@
 export const PORT = parseInt(process.env.TUBECASTER_PORT as string) || 8080
 
-export const API_KEY = (() => {
-  const key = process.env.TUBECASTER_YOUTUBE_API_KEY as string | undefined
-  if (!key) throw new Error("process.env.TUBECASTER_YOUTUBE_API_KEY is needed")
-  return key
-})()
-
 export const CACHE = process.env.TUBECASTER_CACHE !== "no"
 
 export const CACHE_FRONTEND_HOME_SECONDS =
