dcd294e299a28117e5411724be7661019525e093
Carlos Precioso <kalprestito@gmail.com>
2020-02-14 10:04:01 +0100

    Alias lodash to lodash-es

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const withLess = require("@zeit/next-less")
 
 module.exports = withLess(
   withCSS({
-    target: "serverless"
+    target: "serverless",
+    webpack: config => {
+      ;(
+        (config.resolve || (config.resolve = {})).alias ||
+        (config.resolve.alias = {})
+      ).lodash = "lodash-es"
+      return config
+    }
   })
 )
