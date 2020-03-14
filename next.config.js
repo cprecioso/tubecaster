bc41a95c61e64e3a11e82b27f31e82ffd3d0ce9b
Carlos Precioso <kalprestito@gmail.com>
2020-03-15 00:37:30 +0100

    Revert "Migrate to yarn 2"
    This reverts commit bbeeccf23034fffff3353de595784ab793b305c3.

--- next.config.js
+++ next.config.js
@@ -1,16 +1,2 @@
-const PnpWebpackPlugin = require("pnp-webpack-plugin")
 
-module.exports = {
-  target: "serverless",
-  webpack(config) {
-    config.resolve = config.resolve || {}
-    config.resolve.plugins = config.resolve.plugins || []
-    config.resolve.plugins.push(PnpWebpackPlugin)
-
-    config.resolveLoader = config.resolveLoader || {}
-    config.resolveLoader.plugins = config.resolveLoader.plugins || []
-    config.resolveLoader.plugins.push(PnpWebpackPlugin.moduleLoader(module))
-
-    return config
-  }
-}
+module.exports = { target: "serverless" }
