0ed4ade28ac2065b8053e37a4e1d1f02f130254a
Carlos Precioso <kalprestito@gmail.com>
2020-03-14 23:38:58 +0100

    Remove lodash-es alias

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 const withCSS = require("@zeit/next-css")
 const withLess = require("@zeit/next-less")
 
-module.exports = withLess(
-  withCSS({
-    target: "serverless",
-    webpack: config => {
-      ;(
-        (config.resolve || (config.resolve = {})).alias ||
-        (config.resolve.alias = {})
-      ).lodash = "lodash-es"
-      return config
-    }
-  })
-)
+module.exports = withLess(withCSS({ target: "serverless" }))
