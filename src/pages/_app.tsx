85ade7a8ff25b2f0be45fa85591801986b869af3
Carlos Precioso <kalprestito@gmail.com>
2020-05-20 01:28:26 +0200

    Turn TubecasterApp to FunctionComponent

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,44 +1,41 @@
-import App from "next/app"
+import { AppProps } from "next/app"
 import Head from "next/head"
 import Link from "next/link"
 import "picnic"
-import React from "react"
+import React, { FunctionComponent } from "react"
 import "../styles/main.scss"
 
-export default class Tubecaster extends App {
-  render() {
-    const { Component, pageProps } = this.props
-    return (
-      <>
-        <Head>
-          <title>Tubecaster</title>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-        </Head>
-        <header>
-          <h1 className="full">
-            <Link href="/" prefetch={false}>
-              <a>Tubecaster</a>
-            </Link>
-          </h1>
-          <h2 className="full">
-            Turn YouTube playlists and channels into video podcasts
-          </h2>
-        </header>
-        <main>
-          <Component {...pageProps} />
-        </main>
-        <footer className="flex one two-500">
-          <span>
-            Made by <a href="https://precioso.design">Carlos Precioso</a>
-          </span>
-          <span>
-            <a href="https://github.com/cprecioso/tubecaster">Source</a>
-          </span>
-        </footer>
-      </>
-    )
-  }
-}
+const TubecasterApp: FunctionComponent<AppProps> = ({
+  Component,
+  pageProps,
+}) => (
+  <>
+    <Head>
+      <title>Tubecaster</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
+    <header>
+      <h1 className="full">
+        <Link href="/" prefetch={false}>
+          <a>Tubecaster</a>
+        </Link>
+      </h1>
+      <h2 className="full">
+        Turn YouTube playlists and channels into video podcasts
+      </h2>
+    </header>
+    <main>
+      <Component {...pageProps} />
+    </main>
+    <footer className="flex one two-500">
+      <span>
+        Made by <a href="https://precioso.design">Carlos Precioso</a>
+      </span>
+      <span>
+        <a href="https://github.com/cprecioso/tubecaster">Source</a>
+      </span>
+    </footer>
+  </>
+)
+
+export default TubecasterApp
