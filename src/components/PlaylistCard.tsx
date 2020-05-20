3bd430240f5897d5ec1f25af13328cb4ddf4ec2f
Carlos Precioso <kalprestito@gmail.com>
2020-05-20 13:26:16 +0200

    Instructional text for noscript users

--- src/components/PlaylistCard.tsx
+++ src/components/PlaylistCard.tsx
@@ -4,15 +4,17 @@ import { PlaylistData } from "../api/types"
 export const PlaylistCard: React.FunctionComponent<{
   data: PlaylistData
 }> = ({ data }) => {
-  const [podcastUrl, setPodcastUrl] = React.useState(
-    `/api/podcast?id=${data.playlistId}`
-  )
+  const [podcastLink, setPodcastLink] = React.useState({
+    url: `/api/podcast?id=${data.playlistId}`,
+    text: "Right click (or hold) to copy the link",
+    shoxExtraInfo: false,
+  })
   React.useEffect(
     () =>
-      setPodcastUrl((oldUrl) => {
-        const newUrl = new URL(oldUrl, window.location.href)
+      setPodcastLink(({ url }) => {
+        const newUrl = new URL(url, window.location.href)
         newUrl.protocol = "podcast"
-        return newUrl.href
+        return { url: newUrl.href, text: newUrl.href, shoxExtraInfo: true }
       }),
     []
   )
@@ -29,12 +31,18 @@ export const PlaylistCard: React.FunctionComponent<{
         </h4>
       </header>
       <main className="full">
-        <a href={podcastUrl}>
-          <pre className="full">{podcastUrl}</pre>
+        <a href={podcastLink.url}>
+          <pre className="full">{podcastLink.text}</pre>
         </a>
         <p className="full">
-          Copy the link and paste it into your podcast app. You can press and
-          hold (or right-click) the link in order to copy it more easily.
+          <>Copy the link and paste it into your podcast app.</>
+          {podcastLink.shoxExtraInfo ? (
+            <>
+              {" "}
+              You can press and hold (or right-click) the link in order to copy
+              it more easily.
+            </>
+          ) : null}
         </p>
       </main>
     </div>
