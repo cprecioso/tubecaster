e30bed41e3ecc47dc402b6056f694df758a95e50
Carlos Precioso <kalprestito@gmail.com>
2020-05-20 13:15:22 +0200

    Add automatic refresh for noscript

--- src/components/LoadingCard.tsx
+++ src/components/LoadingCard.tsx
@@ -1,5 +1,13 @@
+import Head from "next/head"
 import React from "react"
 
 export const LoadingCard: React.FunctionComponent = () => (
-  <div className="card flex one center">Loading...</div>
+  <>
+    <div className="card flex one center">Loading...</div>
+    <Head>
+      <noscript>
+        <meta key="refresh" httpEquiv="refresh" content="3" />
+      </noscript>
+    </Head>
+  </>
 )
