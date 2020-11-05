200e07b767f981b8b066475387461bd3ae1a54cb
Carlos Precioso <511681+cprecioso@users.noreply.github.com>
2020-11-05 11:07:02 +0000

    Remove noscript fallback

--- src/components/LoadingCard.tsx
+++ src/components/LoadingCard.tsx
@@ -1,13 +1,7 @@
-import Head from "next/head"
 import React from "react"
 
 export const LoadingCard: React.FunctionComponent = () => (
   <>
     <div className="card flex one center">Loading...</div>
-    <Head>
-      <noscript>
-        <meta key="refresh" httpEquiv="refresh" content="3" />
-      </noscript>
-    </Head>
   </>
 )
