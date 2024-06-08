449b132904181129782021c4161dcbe087da3bbb
Carlos Precioso <github@precioso.design>
2024-06-08 08:37:03 +0200

    chore: remove unnecessary loading page

--- src/app/[locale]/channel/[id]/page.tsx
+++ src/app/[locale]/channel/[id]/page.tsx
@@ -1,10 +1,17 @@
-import { Suspense } from "react";
 import { ChannelReference, VideoCollectionType } from "../../../../api/types";
 import { channelReferenceToPlaylistReferences } from "../../../../api/youtube-urls";
-import { LoadingCard } from "../../../../components/LoadingCard";
 import { PlaylistChooseCard } from "../../../../components/PlaylistChooseCard";
 
-const PlaylistChoosePageContent = async ({ id }: { id: string }) => {
+export type Params = {
+  id: string;
+  locale: string;
+};
+
+export default function PlaylistChoosePage({
+  params: { id },
+}: {
+  params: Params;
+}) {
   const channelReference: ChannelReference = {
     type: VideoCollectionType.Channel,
     id,
@@ -14,16 +21,4 @@ const PlaylistChoosePageContent = async ({ id }: { id: string }) => {
     channelReferenceToPlaylistReferences(channelReference);
 
   return <PlaylistChooseCard options={playlistReferences} />;
-};
-
-export default function PlaylistChoosePage({
-  params: { id },
-}: {
-  params: { id: string; locale: string };
-}) {
-  return (
-    <Suspense fallback={<LoadingCard />}>
-      <PlaylistChoosePageContent id={id} />
-    </Suspense>
-  );
 }
