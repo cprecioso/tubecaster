766c54a78163183527c181d430058e3744e4b482
Carlos Precioso <github@precioso.design>
2024-06-08 15:12:41 +0200

    feat: sync with system theme

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,6 @@
 import { Box, Container, Flex, Heading, Link, Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
+import { ThemeProvider } from "next-themes";
 import NextLink from "next/link";
 import { notFound } from "next/navigation";
 import { ReactNode } from "react";
@@ -15,54 +16,58 @@ export default function RootLayout({
   if (!ALLOWED_LOCALES.has(locale)) notFound();
 
   return (
-    <html lang={locale}>
+    <html lang={locale} suppressHydrationWarning>
       <head>
         <title>Tubecaster</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
 
       <body>
-        <Theme>
-          <Container size="2">
-            <Flex direction="column" gap="3">
-              <header>
-                <Flex gap="3" align={"baseline"} wrap="wrap">
-                  <Heading as="h1" size="8">
-                    <NextLink
-                      href={`/${locale}`}
-                      prefetch={false}
-                      passHref
-                      legacyBehavior
-                    >
-                      <Link>Tubecaster</Link>
-                    </NextLink>
-                  </Heading>
+        <ThemeProvider attribute="class">
+          <Theme>
+            <Container size="2">
+              <Flex direction="column" gap="3">
+                <header>
+                  <Flex gap="3" align={"baseline"} wrap="wrap">
+                    <Heading as="h1" size="8">
+                      <NextLink
+                        href={`/${locale}`}
+                        prefetch={false}
+                        passHref
+                        legacyBehavior
+                      >
+                        <Link>Tubecaster</Link>
+                      </NextLink>
+                    </Heading>
 
-                  <Heading as="h2" size="4" weight="regular">
-                    Turn YouTube playlists and channels into video podcasts
-                  </Heading>
-                </Flex>
-              </header>
+                    <Heading as="h2" size="4" weight="regular">
+                      Turn YouTube playlists and channels into video podcasts
+                    </Heading>
+                  </Flex>
+                </header>
 
-              <main>{children}</main>
+                <main>{children}</main>
 
-              <footer>
-                <Flex justify="between" wrap="wrap">
-                  <Box as="span">
-                    Made by{" "}
-                    <Link href="https://precioso.design">Carlos Precioso</Link>
-                  </Box>
+                <footer>
+                  <Flex justify="between" wrap="wrap">
+                    <Box as="span">
+                      Made by{" "}
+                      <Link href="https://precioso.design">
+                        Carlos Precioso
+                      </Link>
+                    </Box>
 
-                  <Box as="span">
-                    <Link href="https://github.com/cprecioso/tubecaster">
-                      Source code
-                    </Link>
-                  </Box>
-                </Flex>
-              </footer>
-            </Flex>
-          </Container>
-        </Theme>
+                    <Box as="span">
+                      <Link href="https://github.com/cprecioso/tubecaster">
+                        Source code
+                      </Link>
+                    </Box>
+                  </Flex>
+                </footer>
+              </Flex>
+            </Container>
+          </Theme>
+        </ThemeProvider>
       </body>
     </html>
   );
