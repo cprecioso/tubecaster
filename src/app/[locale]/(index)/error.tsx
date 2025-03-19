5418236049ae456bbd23e8b94166213ebf017d59
Carlos Precioso <github@precioso.design>
2025-03-19 23:53:03 +0100

    perf: use params in client components

--- src/app/[locale]/(index)/error.tsx
+++ src/app/[locale]/(index)/error.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { ErrorCard } from "@/components/ErrorCard";
-import { useParams } from "next/navigation";
 import IndexPage, { Params } from "./page";
 
-export default function ErrorPage({ error }: { error: Error }) {
-  const params = useParams<Params>();
-
+export default function ErrorPage({
+  error,
+  params,
+}: {
+  error: Error;
+  params: Promise<Params>;
+}) {
   return (
     <>
       <IndexPage params={params} />
