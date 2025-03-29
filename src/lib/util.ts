2e7595ad2f04e4bc1f5758c466f9d6a5e3a49c72
Carlos Precioso <github@precioso.design>
2025-03-29 10:23:25 +0100

    fix: fix route type import

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,3 +1,2 @@
-import { Route } from "next";
-
-export const route = <T extends string>(route: Route<T>) => route;
+export const route = <T extends string>(route: import("next").Route<T>) =>
+  route;
